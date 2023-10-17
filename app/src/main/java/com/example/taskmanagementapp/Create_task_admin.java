4802946978b84e9e4b21eaf6bdfb0b3d392d602d
Kuber Singh <kuber301193@gmail.com>
2023-10-17 15:17:59 -0400

    git commit

---
+++ app/src/main/java/com/example/taskmanagementapp/Create_task_admin.java
@@ -0,0 +1,14 @@
+package com.example.taskmanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Create_task_admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_task_admin);
+    }
+}
